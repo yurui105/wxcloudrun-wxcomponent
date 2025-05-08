a6783d2eb3b5f9b9b0533141750986ef659e713c
yurui105 <yurui1005@gmail.com>
2025-05-08 23:28:14 +0800

    enable log to debug

--- api/official-account/service.go
+++ api/official-account/service.go
@@ -22,6 +22,17 @@ func GetOfficialAccount(appid string) (*officialAccount.OfficialAccount, error) 
 	app, err := officialAccount.NewOfficialAccount(&officialAccount.UserConfig{
 		AppID:  appid, // 公众号、小程序的appid
 		Secret: token, //
+
+		Log: officialAccount.Log{
+			Level: "debug",
+			// 可以重定向到你的目录下，如果设置File和Error，默认会在当前目录下的wechat文件夹下生成日志
+			File:   "/log/info.log",
+			Error:  "/log/error.log",
+			Stdout: true, //  是否打印在终端
+		},
+
+		HttpDebug: true,
+		Debug:     false,
 	})
 
 	app.AccessToken.GetCustomToken = func(key string, refresh bool) object.HashMap {
